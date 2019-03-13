20207b3bccaec552becfcdddc75d4623886a120f
antoanetad78 <antoanetad78@gmail.com>
2019-03-13 15:39:58 +0100

    added middleware, secret get in auth routes adjusted to use it

--- auth/routes.js
+++ auth/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const { toJWT, toData } = require('./jwt')
 const User = require('../users/model')
 const bcrypt = require('bcrypt');
+const auth = require('./middleware')
 
 const router = new Router()
 
@@ -41,7 +42,7 @@ router.post('/logins', (req, res, next) => {
               message: 'Password was incorrect'
             })
           }
-          
+
         })
         .catch(err => {
           console.error(err)
@@ -53,30 +54,10 @@ router.post('/logins', (req, res, next) => {
     }
 })
 
-
-router.get('/secret-endpoint', (req, res) => {
-    const auth = req.headers.authorization && req.headers.authorization.split(' ')
-    if (auth && auth[0] === 'Bearer' && auth[1]) {
-      try {
-        const data = toData(auth[1])
-        res.send({
-          message: 'Thanks for visiting the secret endpoint.',
-          data
-        })
-      }
-      catch(error) {
-        res.status(400).send({
-          message: `Error ${error.name}: ${error.message}`,
-        })
-      }
-    }
-    else {
-      res.status(401).send({
-        message: 'Please supply some valid credentials'
-      })
-    }
+router.get('/secret-endpoint', auth, (req, res) => {
+    res.send({
+      message: `Thanks for visiting the secret endpoint ${req.user.email}.`,
+    })
   })
-
-
 
   module.exports = router
