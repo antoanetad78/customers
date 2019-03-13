2de6a76794e17ce90fce8e221acbe0149d60495d
antoanetad78 <antoanetad78@gmail.com>
2019-03-13 15:31:44 +0100

    login and signup endpoints working

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
 const companiesRouter = require('./companies/routes')
+const login = require('./auth/routes')
+const usersRouter = require('./users/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -10,5 +12,7 @@ app
   .use(bodyParser.json())
   .use(customersRouter)
   .use(companiesRouter)
+  .use(login)
+  .use(usersRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
 
